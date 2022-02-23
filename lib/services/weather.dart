174f75023ce71a86dcb51eba4428e74b11cf2f62
Kevin Koo <kevin.koo@vi.app>
2022-02-23 15:56:29 +0800

    fix null safety

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_testing/services/location.dart';
 import 'package:flutter_testing/services/networking.dart';
 
-const apiKey = 'b1c398b302e0589ebe6ba4a862070a2b';
+const apiKey = 'b1c398b302e0589ebe6ba4a862070a2b2';
 const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
 
 class WeatherModel {
@@ -42,9 +42,9 @@ class WeatherModel {
       return '☀️';
     } else if (condition! <= 804) {
       return '☁️';
-    } else {
-      return '🤷‍';
     }
+
+    return '🤷‍';
   }
 
   String getMessage(int temp) {
